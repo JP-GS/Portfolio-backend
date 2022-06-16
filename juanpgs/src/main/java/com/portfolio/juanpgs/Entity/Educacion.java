943a9a7de163398b4b172a379b1573fcc1b003eb
JP-GS <juanpgordillo89@gmail.com>
2022-06-16 19:28:19 -0300

    Creacion de la tabla educacion en el back, todos sus componentes listos

--- juanpgs/src/main/java/com/portfolio/juanpgs/Entity/Educacion.java
+++ juanpgs/src/main/java/com/portfolio/juanpgs/Entity/Educacion.java
@@ -11,23 +11,31 @@ import lombok.Setter;
 
 @Getter @Setter
 @Entity
-public class Persona {
-
+public class Educacion {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @NotNull
-    @Size(min = 1, max = 11, message = "No cumple con la longitud")
+    @Size(min = 1, max = 50, message = "No cumple con la longitud")
     private String nombre;
 
     @NotNull
-    @Size(min = 1, max = 11, message = "No cumple con la longitud")
-    private String apellido;   
-
     @Size(min = 1, max = 50, message = "No cumple con la longitud")
-    private String descripcion; 
+    private String titulo;
 
     @Size(min = 1, max = 50, message = "No cumple con la longitud")
     private String img;
+
+    @Size(min = 1, max = 50, message = "No cumple con la longitud")
+    private String carrera;
+
+    //@NotNull
+    //private Int puntaje;
+
+    @Size(min = 1, max = 4, message = "No cumple con la longitud")
+    private String inicio;
+
+    @Size(min = 1, max = 4, message = "No cumple con la longitud")
+    private String fin;
 }
