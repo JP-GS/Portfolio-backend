be711b5713a2a742de8dcf0ffb97b431453b63e3
JP-GS <juanpgordillo89@gmail.com>
2022-07-19 05:55:59 -0300

    creacion de los repositorios de rol y usuario y comienzo de la creacion de los servicios

---
+++ juanpgs/src/main/java/com/portfolio/juanpgs/Security/Service/RolService.java
@@ -0,0 +1,10 @@
+package com.portfolio.juanpgs.Security.Service;
+
+import javax.transaction.Transactional;
+import org.springframework.stereotype.Service;
+
+@Service
+@Transactional
+public class RolService {
+
+}
