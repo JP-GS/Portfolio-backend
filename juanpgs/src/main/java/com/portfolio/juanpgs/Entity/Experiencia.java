483d92ac6faba911baddc8d1be13ba353b5024e2
JP-GS <juanpgordillo89@gmail.com>
2022-07-25 17:47:20 -0300

    creacion de la entidad, repositorio y servicio de experiencia

---
+++ juanpgs/src/main/java/com/portfolio/juanpgs/Entity/Experiencia.java
@@ -0,0 +1,81 @@
+package com.portfolio.juanpgs.Entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Experiencia {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String nombreExp;
+    private String puesto;
+    private String imgExp;
+    private String inicio;
+    private String fin;
+
+//Constructores
+    public Experiencia() {
+    }
+
+    public Experiencia(String nombreExp, String puesto, String imgExp, String inicio, String fin) {
+        this.nombreExp = nombreExp;
+        this.puesto = puesto;
+        this.imgExp = imgExp;
+        this.inicio = inicio;
+        this.fin = fin;
+    }
+
+//Getters & Setters
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombreExp() {
+        return nombreExp;
+    }
+
+    public void setNombreExp(String nombreExp) {
+        this.nombreExp = nombreExp;
+    }
+
+    public String getPuesto() {
+        return puesto;
+    }
+
+    public void setPuesto(String puesto) {
+        this.puesto = puesto;
+    }
+
+    public String getImgExp() {
+        return imgExp;
+    }
+
+    public void setImgExp(String imgExp) {
+        this.imgExp = imgExp;
+    }
+
+    public String getInicio() {
+        return inicio;
+    }
+
+    public void setInicio(String inicio) {
+        this.inicio = inicio;
+    }
+
+    public String getFin() {
+        return fin;
+    }
+
+    public void setFin(String fin) {
+        this.fin = fin;
+    }
+
+}
