dcfbe3250de927deded0b47745c388f49964770a
JP-GS <juanpgordillo89@gmail.com>
2022-08-19 17:37:12 -0300

    make it better

--- juanpgs/src/main/java/com/portfolio/juanpgs/Security/MainSecurity.java
+++ juanpgs/src/main/java/com/portfolio/juanpgs/Security/MainSecurity.java
@@ -41,7 +41,7 @@ public class MainSecurity extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.cors().and().csrf().disable()
                 .authorizeRequests()
-                .antMatchers("/**").permitAll()
+                .antMatchers("/https://portfoliojpgs-10307.web.app/").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .exceptionHandling().authenticationEntryPoint(jwtEntryPoint)
