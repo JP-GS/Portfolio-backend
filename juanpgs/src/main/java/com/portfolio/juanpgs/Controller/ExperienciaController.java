17570e98295f30f511633b2d2342f5233d9d1a9f
JP-GS <juanpgordillo89@gmail.com>
2022-08-21 15:55:29 -0300

    make it better

--- juanpgs/src/main/java/com/portfolio/juanpgs/Controller/ExperienciaController.java
+++ juanpgs/src/main/java/com/portfolio/juanpgs/Controller/ExperienciaController.java
@@ -21,7 +21,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/experiencia")
-@CrossOrigin(origins = "https://portfoliojpgs-10307.web.app/")
+@CrossOrigin(origins = "/portfoliojpgs-10307.web.app/")
 public class ExperienciaController {
 
     @Autowired
--- juanpgs/src/main/java/com/portfolio/juanpgs/Controller/PersonaController.java
+++ juanpgs/src/main/java/com/portfolio/juanpgs/Controller/PersonaController.java
@@ -22,7 +22,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/persona")
-@CrossOrigin(origins = "https://portfoliojpgs-10307.web.app/")
+@CrossOrigin(origins = "/portfoliojpgs-10307.web.app/")
 
 public class PersonaController {
 
