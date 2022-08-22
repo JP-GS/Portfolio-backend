72f03aafd5765f1ce92823dd6d6ae7445df27533
JP-GS <juanpgordillo89@gmail.com>
2022-08-22 19:34:36 -0300

    make it better

--- juanpgs/src/main/java/com/portfolio/juanpgs/Controller/ProyectoController.java
+++ juanpgs/src/main/java/com/portfolio/juanpgs/Controller/ProyectoController.java
@@ -21,7 +21,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/proyecto")
-@CrossOrigin(origins = "https://portfoliojpgs-10307.web.app")
+@CrossOrigin(origins = "*")
 public class ProyectoController {
 
     @Autowired
